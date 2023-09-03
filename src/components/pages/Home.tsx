280b51b211b0a2532c9673c9a1e9e58100d377db
rit-ofc <rit-ofc@pop-os.localdomain>
2023-09-03 19:56:48 +0545

    tailwind css

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,9 +5,10 @@ function Home() {
   const handleClick = () => {
     alert('Clicked');
   };
+
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold text-red-600">Hello World</h1>
       <NavLink to="/about">About</NavLink>;
       <Button onClick={handleClick} type="button">
         Click Me
